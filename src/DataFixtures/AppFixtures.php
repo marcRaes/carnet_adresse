7c24caf6e384c9c2c9272fb227ea6d6e8ac0c010
marcRaes <contact@marcraes.fr>
2025-04-06 16:33:34 +0200

    Ajout des colonnes adresse et mail

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,34 +6,50 @@ use App\Entity\Contact;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    private const NUM_CONTACTS = 50;
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
+        $data = $this->getData($faker);
 
-        for ($i = 0; $i < 50; $i++) {
-            $contact = $this->createContact(
-                $faker->lastName(),
-                $faker->firstName(),
-                $faker->phoneNumber(),
-            );
-
+        foreach ($data as $contactData) {
+            $contact = $this->createContact($contactData);
             $manager->persist($contact);
         }
 
         $manager->flush();
     }
 
-    private function createContact(string $nom, string $prenom, string $telephone): Contact
+    private function getData(Generator $faker): array
+    {
+        $data = [];
+        for ($i = 0; $i <= self::NUM_CONTACTS; $i++) {
+            $data[] = [
+                'nom' => $faker->lastName(),
+                'prenom' => $faker->firstName(),
+                'telephone' => $faker->phoneNumber(),
+                'adresse' => $faker->address(),
+                'mail' => $faker->email(),
+            ];
+        }
+
+        return $data;
+    }
+
+    private function createContact(array $data): Contact
     {
         $contact = new Contact();
 
-        $contact->setNom($nom)
-            ->setPrenom($prenom)
-            ->setTelephone($telephone)
-        ;
+        $contact->setNom($data['nom'])
+            ->setPrenom($data['prenom'])
+            ->setTelephone($data['telephone'])
+            ->setAdresse($data['adresse'])
+            ->setMail($data['mail']);
 
         return $contact;
     }
--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -22,6 +22,12 @@ class Contact
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $telephone = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $mail = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -59,6 +65,30 @@ class Contact
     public function setTelephone(?string $telephone): static
     {
         $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(?string $mail): static
+    {
+        $this->mail = $mail;
 
         return $this;
     }
